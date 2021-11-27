de7ee3495689f2c7f468383bf689cf947670e99f
unknown <angelaescariocorpuz@gmail.com>
2021-11-28 07:30:07 +0800

    function for the program introduction

--- program 2 - password validator.py
+++ program 2 - password validator.py
@@ -8,3 +8,23 @@
 #ex.
 #input: P@ssw0rd+P@ssw0rd
 #ouput: Valid
+
+import time
+
+def passwordintro ():
+    print ('Hello and welcome to Password Validator!')
+    time.sleep (1)
+    print ("Please enter the password that you want to validate and we'll check if it's valid or not.")
+    time.sleep (1)
+    print ('For your password to be valid, here are some guidelines:')
+    time.sleep (1)
+    print ('1. Your password must be greater than 15 letters.')
+    time.sleep (1)
+    print ('2. Your password must have at least one capital letter.')
+    time.sleep (1)
+    print ('3. Your password must have at least one number (0-9).')
+    time.sleep (1)
+    print ('4. Your password must have atleast one special character (@,#,!,%,^,&,*, etc.).')
+    time.sleep (1)
+    print ("That's all for the instructions, you can now enter your password.")
+
