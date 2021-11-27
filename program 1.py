6a5d19b6e5797f597698eda6ce7ed404f530a668
unknown <angelaescariocorpuz@gmail.com>
2021-11-27 18:11:09 +0800

    insertion of code where the user can input their sentence

---
+++ program 1.py
@@ -0,0 +1,17 @@
+#Program 1:
+#Create a program that ask for a sentence as input.
+#Display the number of words, vowels and consonants in the input 
+#ex.
+#input: Bahala kayo dyan
+#output:
+#words: 3
+#vowels: 6
+#consonants: 8
+
+import time
+
+print ("Welcome to word, vowel, and consonant counter!")
+time.sleep (2)
+print ("Please enter your sentence for the program to count the amount of words, vowels, and consonants.")
+time.sleep (2)
+sentence = input("Enter sentence here: ")
